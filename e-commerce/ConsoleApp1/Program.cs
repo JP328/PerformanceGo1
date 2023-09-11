c7f901451cc241c4ce86b0f8a418f2431a2a8e9f
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-09-11 14:36:16 -0300

    começando o menu

--- e-commerce/ConsoleApp1/Program.cs
+++ e-commerce/ConsoleApp1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? name, brand, description, gender, rules, acessories, typeOfGame;
+            decimal price;
+
+            while (true)
+            {
+
+
+            }
         }
     }
 }
